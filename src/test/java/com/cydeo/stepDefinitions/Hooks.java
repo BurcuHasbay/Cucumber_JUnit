fe2bbc1c2d54b135cc6139e9fb56c17adb281ece
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-26 14:15:25 +0300

    Hook Class

---
+++ src/test/java/com/cydeo/stepDefinitions/Hooks.java
@@ -0,0 +1,25 @@
+package com.cydeo.stepDefinitions;
+
+
+import org.junit.Before;
+
+import java.beans.BeanProperty;
+
+/*
+In this class, we will be able to pass pre- & post-conditions to each scenario and each step
+ */
+
+/*
+Import from io.cucumber.java not from JUNIT
+ */
+public class Hooks {
+    @Before
+    public void setUpMethod(){
+
+
+
+
+
+
+    }
+}
