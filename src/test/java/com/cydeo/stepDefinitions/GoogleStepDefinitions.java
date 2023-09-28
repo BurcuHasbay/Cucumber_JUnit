01039092b3789210212edbb19e08a33ca6e430ec
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-28 13:28:42 +0300

    sendKeys

--- src/test/java/com/cydeo/stepDefinitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/GoogleStepDefinitions.java
@@ -5,6 +5,7 @@ import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 import java.lang.ref.SoftReference;
 
@@ -32,7 +33,7 @@ public class GoogleStepDefinitions {
 
     @When("user types apple and clicks enter")
     public void userTypesAppleAndClicksEnter() {
-        googleSearchPage.searchBox.sendKeys("apple");
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
 
     @Then("user sees apple in the google title")
