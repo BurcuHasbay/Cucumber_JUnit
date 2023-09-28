d2e69f9f632209b900b99b6641a2672a4e5a2d0e
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-28 13:19:35 +0300

    google Search Page POM

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -4,11 +4,15 @@ import com.cydeo.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleSearchPage {
+
+    // Create constructor first
+    // Then initialize the driver instance and this class' instance using PageFactory.initElements
+
     public GoogleSearchPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-
+//We can now start locating Web Elements using @FindBy annotation
 
 
 }
