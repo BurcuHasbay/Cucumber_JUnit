589af7e4d95407c258e5ccf3512fa09fae939a79
burcuhasbay <burcu.hasbay@gmail.com>
2023-09-28 13:22:22 +0300

    google Search Page POM-locator

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class GoogleSearchPage {
@@ -14,5 +16,7 @@ public class GoogleSearchPage {
 
 //We can now start locating Web Elements using @FindBy annotation
 
+    @FindBy(name = "q")
+    public WebElement searchBox;
 
 }
