0860a7e56ab5dafdb0922651891fc4a1fbf588b5
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-02 13:39:16 +0300

    parameterization:Cucumber expression

--- src/test/java/com/cydeo/stepDefinitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/GoogleStepDefinitions.java
@@ -16,6 +16,14 @@ public class GoogleStepDefinitions {
     //
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+    @When("user types {string} and clicks enter")
+    public void user_types_and_clicks_enter(String string) {
+
+    }
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String string) {
+
+    }
 
 
     @When("user is on Google search page")
