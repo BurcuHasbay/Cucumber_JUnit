5e4f05e05f66fa210f8539820c4234ae74e2e9ac
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-03 17:20:00 +0300

    Wiki POM

--- src/test/java/com/cydeo/pages/WikiSearchPage.java
+++ src/test/java/com/cydeo/pages/WikiSearchPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class WikiSearchPage {
@@ -8,5 +10,10 @@ public class WikiSearchPage {
     public WikiSearchPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(id = "searchInput")
+    public WebElement searchBox;
+
+
 
 }
