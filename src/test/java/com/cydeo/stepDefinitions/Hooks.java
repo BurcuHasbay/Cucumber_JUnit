ca0143501eb46930d4f1e901822f31e6b23222e3
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-03 15:19:16 +0300

    Hooks-TakeScreenShot

--- src/test/java/com/cydeo/stepDefinitions/Hooks.java
+++ src/test/java/com/cydeo/stepDefinitions/Hooks.java
@@ -6,6 +6,8 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.beans.BeanProperty;
 
@@ -27,6 +29,9 @@ public class Hooks {
 
  @After
     public void tearDownScenario(){
+
+       byte[]screenshot = ((TakesScreenshot)Driver.getDriver())
+               .getScreenshotAs(OutputType.BYTES);
 
      Driver.closeDriver();
 
