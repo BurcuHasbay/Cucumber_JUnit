99c0b34bdf66502c8c2f7ad83ed8581be42a716d
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-04 13:31:44 +0300

    Wiki Task step definitions

--- src/test/java/com/cydeo/stepDefinitions/Wikipedia_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/Wikipedia_StepDefinitions.java
@@ -2,40 +2,44 @@ package com.cydeo.stepDefinitions;
 
 import com.cydeo.pages.WikipediaPage;
 import com.cydeo.utilities.Driver;
-import io.cucumber.java.bs.A;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.Keys;
 
 public class Wikipedia_StepDefinitions {
 
-    WikipediaPage wikipediaPage = new WikipediaPage();
+WikipediaPage wikipediaPage = new WikipediaPage();
+
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
-        Driver.getDriver().get("https://www.wikipedia.org/");
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-    @When("User types Steve Jobs in the wiki search box")
-    public void user_types_steve_jobs_in_the_wiki_search_box() {
-        wikipediaPage.searchBox.sendKeys("Steve Job"+ Keys.ENTER);
+    @When("User types {string} in the wiki search box")
+    public void user_types_in_the_wiki_search_box(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
-        wikipediaPage.searchButton.click();
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
-    @Then("User sees Steve Jobs is in the wiki title")
-    public void user_sees_steve_jobs_is_in_the_wiki_title() {
-
-    }
-
-    @When("User types {string} in the wiki search box")
-    public void userTypesInTheWikiSearchBox(String arg0) {
-
-    }
-
     @Then("User sees {string} is in the wiki title")
-    public void userSeesIsInTheWikiTitle(String string) {
-        Assert.assertTrue(Driver.getDriver().getTitle().contains(string));
+    public void user_sees_is_in_the_wiki_title(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
+
+
+
+
+
+
+
+
+
+
+
 }
