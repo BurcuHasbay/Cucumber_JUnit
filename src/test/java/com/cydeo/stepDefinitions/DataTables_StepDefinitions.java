c9906268f305a0ea107ea2193506e9e501794b16
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-04 16:01:40 +0300

    Data Tables StepDefinitions

--- src/test/java/com/cydeo/stepDefinitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/DataTables_StepDefinitions.java
@@ -11,6 +11,9 @@ public class DataTables_StepDefinitions {
     @Then("user should see the fruits I like")
     public void user_should_see_the_fruits_i_like(List<String>fruits) {
 
+        System.out.println(fruits);
+        System.out.println(fruits.get(1));
+
     }
 
 }
