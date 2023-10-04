f7b5b469115c0f5452d0013575fcbae20c891d70
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-04 12:52:00 +0300

    Wiki Page

---
+++ src/test/java/com/cydeo/pages/WikipediaPage.java
@@ -0,0 +1,8 @@
+package com.cydeo.pages;
+
+public class WikipediaPage {
+
+    // Create constructor first
+    // Then initialize the driver instance and this class' instance using PageFactory.initElements
+
+}
