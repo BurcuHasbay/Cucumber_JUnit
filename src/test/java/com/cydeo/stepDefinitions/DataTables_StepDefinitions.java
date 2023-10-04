4de1df0a6f57088e103fd45932836b914432db91
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-04 15:45:48 +0300

    Data Tables StepDefinitions

--- src/test/java/com/cydeo/stepDefinitions/DataTables_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/DataTables_StepDefinitions.java
@@ -2,20 +2,15 @@ package com.cydeo.stepDefinitions;
 
 import io.cucumber.java.en.Then;
 
+import java.util.List;
+
 public class DataTables_StepDefinitions {
 
 
 
     @Then("user should see the fruits I like")
-    public void user_should_see_the_fruits_i_like(io.cucumber.datatable.DataTable dataTable) {
-        // Write code here that turns the phrase above into concrete actions
-        // For automatic transformation, change DataTable to one of
-        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
-        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
-        // Double, Byte, Short, Long, BigInteger or BigDecimal.
-        //
-        // For other transformations you can register a DataTableType.
-        throw new io.cucumber.java.PendingException();
+    public void user_should_see_the_fruits_i_like(List<String>fruits) {
+
     }
 
 }
