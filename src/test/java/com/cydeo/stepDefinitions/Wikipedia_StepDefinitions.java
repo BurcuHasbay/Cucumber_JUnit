d03b9d5e60619a9fcecd63dbb8334e26612e9550
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-04 13:33:15 +0300

    Wiki Task step definitions

--- src/test/java/com/cydeo/stepDefinitions/Wikipedia_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/Wikipedia_StepDefinitions.java
@@ -6,29 +6,27 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import java.sql.DriverPropertyInfo;
+
 public class Wikipedia_StepDefinitions {
 
 WikipediaPage wikipediaPage = new WikipediaPage();
 
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get("https://www.wikipedia.org/");
     }
     @When("User types {string} in the wiki search box")
     public void user_types_in_the_wiki_search_box(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        wikipediaPage.searchBox.sendKeys(string);
     }
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("User sees {string} is in the wiki title")
     public void user_sees_is_in_the_wiki_title(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
