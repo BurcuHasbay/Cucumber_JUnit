0b12f58ee20acf89a6cd8b585a0c515104fb271d
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-07 15:42:45 +0300

    Configuration Properties this time to go to that page

--- src/test/java/com/cydeo/stepDefinitions/WebTable_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/WebTable_StepDefinitions.java
@@ -1,5 +1,7 @@
 package com.cydeo.stepDefinitions;
 
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,7 +10,8 @@ import io.cucumber.java.en.When;
 public class WebTable_StepDefinitions {
     @Given("User is on the login page of web table app")
     public void user_is_on_the_login_page_of_web_table_app() {
-
+        String url = ConfigurationReader.getProperty("web.table.url");
+        Driver.getDriver().get(url);
     }
 
 
