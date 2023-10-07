a8d54290793bb69d14f0587e9d862e54476ff179
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-07 15:50:29 +0300

    WebTable POM-Creating Object

--- src/test/java/com/cydeo/pages/WebTablePage.java
+++ src/test/java/com/cydeo/pages/WebTablePage.java
@@ -1,6 +1,17 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
 public class WebTablePage {
+
+public WebTablePage(){
+    PageFactory.initElements(Driver.getDriver(),this);
+}
+
+
+
+
 
 
 }
