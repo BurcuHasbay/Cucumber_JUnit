c960359f951c5a47ab0a3ef509a8ab06816b40b9
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-08 15:16:49 +0300

    DataTables DropDownPage

---
+++ src/test/java/com/cydeo/pages/DropDownsPage.java
@@ -0,0 +1,14 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class DropDownsPage {
+    public DropDownsPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+
+}
