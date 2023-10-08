3d35c4ba5a817db2418200bcc53f1949c12f9a2c
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-08 15:19:18 +0300

    DataTables DropDownPage

--- src/test/java/com/cydeo/pages/DropDownsPage.java
+++ src/test/java/com/cydeo/pages/DropDownsPage.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DropDownsPage {
@@ -8,7 +10,8 @@ public class DropDownsPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-
+@FindBy(id = "month")
+    public WebElement monthDropDown;
 
 
 }
