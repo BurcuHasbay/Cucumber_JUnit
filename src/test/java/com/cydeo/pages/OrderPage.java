cc4c72ca01a805143266a9d356543c1902a68a36
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-09 15:36:45 +0300

    Order PAge

---
+++ src/test/java/com/cydeo/pages/OrderPage.java
@@ -0,0 +1,11 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrderPage {
+
+    public OrderPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
