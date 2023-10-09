42d579de24da368dcde4717bc3367a8890cc6e09
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-09 14:38:52 +0300

    WebTable Order of Step-definition

--- src/test/java/com/cydeo/stepDefinitions/WebTableOrder_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/WebTableOrder_StepDefinitions.java
@@ -1,8 +1,63 @@
 package com.cydeo.stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class WebTableOrder_StepDefinitions {
 
+    @Given("User is already logged in and on order page")
+    public void user_is_already_logged_in_and_on_order_page() {
 
+    }
+    @When("User selects product type {string}")
+    public void user_selects_product_type(String string) {
+
+    }
+    @When("User enters quantity {string}")
+    public void user_enters_quantity(String string) {
+
+    }
+    @When("User enters costumer name {string}")
+    public void user_enters_costumer_name(String string) {
+
+    }
+    @When("User enters street {string}")
+    public void user_enters_street(String string) {
+
+    }
+    @When("User enters city {string}")
+    public void user_enters_city(String string) {
+
+    }
+    @When("User enters state {string}")
+    public void user_enters_state(String string) {
+
+    }
+    @When("User enters zipcode {string}")
+    public void user_enters_zipcode(String string) {
+
+    }
+    @When("User selects credit card type {string}")
+    public void user_selects_credit_card_type(String string) {
+
+    }
+    @When("User enters credit card number {string}")
+    public void user_enters_credit_card_number(String string) {
+
+    }
+    @When("User enters expiry date {string}")
+    public void user_enters_expiry_date(String string) {
+
+    }
+    @When("User enters process order button")
+    public void user_enters_process_order_button() {
+
+    }
+    @Then("User should see {string} in first row of the web table")
+    public void user_should_see_in_first_row_of_the_web_table(String string) {
+
+    }
 
 
 
