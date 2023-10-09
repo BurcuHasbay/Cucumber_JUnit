f699a92d56ce06deab83b8e4148ed71477d965b3
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-09 15:15:54 +0300

    name changed for Page

--- src/test/java/com/cydeo/pages/BasePage.java
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -3,9 +3,9 @@ package com.cydeo.pages;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class WebTableOrderPage {
+public class BasePage {
 
-    public WebTableOrderPage){
+    public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
