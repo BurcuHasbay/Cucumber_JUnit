99fd8ec86c9867808f6997409d7a993bbabb961e
burcuhasbay <burcu.hasbay@gmail.com>
2023-10-09 15:18:48 +0300

    Storing WebElements in BasePage

--- src/test/java/com/cydeo/pages/BasePage.java
+++ src/test/java/com/cydeo/pages/BasePage.java
@@ -1,14 +1,21 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+
+/*
+In this class, we will store WebElements to All Pages
+ */
 public class BasePage {
 
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-
+    @FindBy()
+    public WebElement viewAllOrders;
 
 }
