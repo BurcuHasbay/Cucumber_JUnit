35e0531383cace6957e732749e9712c648344b65
burcuhasbay <burcu.hasbay@gmail.com>
2023-12-16 14:59:54 +0300

    Finding locators

--- src/test/java/com/cydeo/pages/AmazonPage.java
+++ src/test/java/com/cydeo/pages/AmazonPage.java
@@ -1,4 +1,22 @@
 package com.cydeo.pages;
 
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class AmazonPage {
+
+
+    //// Create constructor first
+    public AmazonPage (){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//div[@class='nav-line-1-container']")
+    public WebElement logInContainerBox;
+
+
+
+
 }
