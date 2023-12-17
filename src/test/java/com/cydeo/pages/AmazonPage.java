f047ec5efa6b9c8cc1099140e53b4273b33a215b
burcuhasbay <burcu.hasbay@gmail.com>
2023-12-17 20:50:50 +0300

    locators

--- src/test/java/com/cydeo/pages/AmazonPage.java
+++ src/test/java/com/cydeo/pages/AmazonPage.java
@@ -1,6 +1,7 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import io.cucumber.java.de.Wenn;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -19,11 +20,39 @@ public class AmazonPage {
     @FindBy(id = "nav-link-accountList-nav-line-1")
     public WebElement logInContainerBox;
 
-    @FindBy(id = "nav-flyout-ya-newCust")
-    public WebElement areYouNew;
 
-    @FindBy(xpath = "//*[@id=\"nav-flyout-ya-newCust\"]/a")
-    public WebElement BeANewMemberLink;
+    @FindBy(xpath ="//a[@class='nav-a']" )
+    public WebElement UyeOlunLink;
+
+    @FindBy(xpath = "//div[@class='nav_pop_new_cust nav-flyout-content nav-flyout-accessibility']")
+    public WebElement newMemberMessage;
+
+   // @FindBy(partialLinkText = "Üye olun.")
+    //public WebElement UyeOlunLink;
+
+    @FindBy(id = "ap_customer_name")
+    public WebElement userNameLastNameBox;
+
+    @FindBy(xpath = "//*[@id='ap_email']")
+    public WebElement emailBox;
+
+    @FindBy(xpath = "//*[@id='ap_password']")
+    public WebElement passwordBox;
+
+    @FindBy(xpath = "//input[@type='checkbox']")
+    public WebElement checkBox;
+
+    @FindBy(xpath ="//*[@id='ap_password_check']")
+    public WebElement passwordBox2;
+
+    @FindBy(xpath = "//*[@id='continue']")
+    public WebElement continueButton;
+
+
+
+
+
+
 
 
 
