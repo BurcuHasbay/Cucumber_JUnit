d5cdb08c35521474f6fb2e2887ac986da7873903
burcuhasbay <burcu.hasbay@gmail.com>
2023-12-18 14:36:54 +0300

    Amazon LogIn Account StepDef setting

---
+++ src/test/java/com/cydeo/stepDefinitions/AmazonLogInAccount_StepDefinitions.java
@@ -0,0 +1,29 @@
+package com.cydeo.stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class AmazonLogInAccount_StepDefinitions {
+
+
+    @Given("User is on the homepage of the website and try to log-in")
+    public void userIsOnTheHomepageOfTheWebsiteAndTryToLogIn() {
+
+    }
+    @When("User is on the login page of Amazon")
+    public void user_is_on_the_login_page_of_amazon() {
+
+    }
+    @And("User types {string} in the email input box")
+    public void user_types_in_the_email_input_box(String string) {
+
+    }
+    @And("User clicks the button to continue")
+    public void user_clicks_the_button_to_continue() {
+
+    }
+
+
+
+}
