a94b6edcc6753ef223d0374e4ec2ec72ee613f80
burcuhasbay <burcu.hasbay@gmail.com>
2023-12-18 13:33:06 +0300

    alternatives were shadowed

--- src/test/java/com/cydeo/stepDefinitions/Amazon_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/Amazon_StepDefinitions.java
@@ -18,9 +18,9 @@ public class Amazon_StepDefinitions {
 
     Actions actions = new Actions(Driver.getDriver());
 
-    Faker faker = new Faker();
+   // Faker faker = new Faker();
 
-    String generatedPassword = faker.internet().password();
+    //String generatedPassword = faker.internet().password();
 
 
     @When("User is on the Amazon home page")
@@ -55,25 +55,67 @@ public class Amazon_StepDefinitions {
 
     }
 
-    @When("User types first name and las name in the name box")
+    @When("User types first name and last name as {string} in the name box")
+    public void userTypesFirstNameAndLastNameAsInTheNameBox(String string) {
+        amazonPage.userNameLastNameBox.sendKeys(string);
+    }
+
+    @And("User types {string}  in the e-mail box")
+    public void userTypesInTheEMailBox(String string) {
+        amazonPage.emailBox.sendKeys(string);
+    }
+
+    @And("User types the password as {string} in the password box")
+    public void userTypesThePasswordAsInThePasswordBox(String string) {
+        amazonPage.passwordBox.sendKeys(string);
+    }
+
+    @And("User types the same password {string} in the password")
+    public void userTypesTheSamePasswordInThePassword(String string) {
+        amazonPage.passwordBox2.sendKeys(string);
+    }
+
+    @And("User clicks the checkbox button")
+    public void userClicksTheCheckboxButton() {
+        amazonPage.checkBox.click();
+    }
+
+    @Then("User clicks the button to create an account")
+    public void userClicksTheButtonToCreateAnAccount() {
+        amazonPage.continueButton.click();
+    }
+
+    /* @When("User types first name and las name in the name box")
     public void user_types_first_name_and_las_name_in_the_name_box() {
         amazonPage.userNameLastNameBox.sendKeys(faker.name().firstName(),faker.name().lastName());
     }
-    @When("User types email  in the e-mail box")
+    */
+
+
+   /* @When("User types email  in the e-mail box")
     public void user_types_email_in_the_e_mail_box() {
     amazonPage.emailBox.sendKeys(faker.internet().emailAddress());
     }
-    @When("User types a password in the password box")
+    */
+
+
+   /* @When("User types a password in the password box")
     public void user_types_a_password_in_the_password_box() {
     amazonPage.passwordBox.sendKeys(generatedPassword);
     }
-    @When("User types the same password in the password box to check")
+
+    */
+   /* @When("User types the same password in the password box to check")
     public void user_types_the_same_password_in_the_password_box_to_check() {
     amazonPage.passwordBox2.sendKeys(generatedPassword);
     }
-    @Then("User clicks the button to create an account")
+
+    */
+  /*  @Then("User clicks the button to create an account")
     public void user_clicks_the_button_to_create_an_account() {
     amazonPage.continueButton.click();
     }
+
+   */
 
 }
