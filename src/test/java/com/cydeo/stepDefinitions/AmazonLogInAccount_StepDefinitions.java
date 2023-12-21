482df6258ac6229cd7edbd14ffaed1a29df14a9b
burcuhasbay <burcu.hasbay@gmail.com>
2023-12-21 11:50:23 +0300

    Edited steps of the new Scenario for Amazon's functionality

--- src/test/java/com/cydeo/stepDefinitions/AmazonLogInAccount_StepDefinitions.java
+++ src/test/java/com/cydeo/stepDefinitions/AmazonLogInAccount_StepDefinitions.java
@@ -6,10 +6,19 @@ import com.cydeo.pages.DropDownsPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.junit.rules.ExpectedException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class AmazonLogInAccount_StepDefinitions {
 
@@ -18,8 +27,9 @@ public class AmazonLogInAccount_StepDefinitions {
     AmazonLogInAccountPage amazonLogInAccountPage = new AmazonLogInAccountPage();
 
     Actions actions = new Actions(Driver.getDriver());
+    JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
-
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(),Duration.ofSeconds(10));
     @Given("User is on the homepage of the website and try to log-in")
     public void userIsOnTheHomepageOfTheWebsiteAndTryToLogIn() {
         Driver.getDriver().get("https://www.amazon.com.tr/");
@@ -68,7 +78,48 @@ public class AmazonLogInAccount_StepDefinitions {
 
     @And("User types {string} and click to search")
     public void userTypesAndClickToSearch(String string) {
-    amazonLogInAccountPage.searchBox.sendKeys(string).
-    amazonLogInAccountPage.searchBox.click();
+    amazonLogInAccountPage.searchBox.sendKeys(string);
+    amazonLogInAccountPage.searchBoxClickButton.click();
+
+
+    }
+
+    @And("User scrolls down till the next button")
+    public void userScrollsDownTillTheNextButton() {
+        js.executeScript("arguments[0].scrollIntoView(true)", amazonLogInAccountPage.nextButton);
+    }
+
+    @And("User scrolls up till the search box")
+    public void userScrollsUpTillTheSearchBox() {
+        js.executeScript("arguments[0].scrollIntoView(true)", amazonLogInAccountPage.searchBox);
+    }
+
+
+    @And("User deletes the the previous typed one and types {string} in the search box and clicks")
+    public void userDeletesTheThePreviousTypedOneAndTypesInTheSearchBoxAndClicks(String string) {
+        amazonLogInAccountPage.searchBox.clear();
+        amazonLogInAccountPage.searchBox.sendKeys(string);
+        amazonLogInAccountPage.searchBoxClickButton.click();
+    }
+
+
+    @When("User scrolls down till the dart he or she selects then clicks on it")
+    public void userScrollsDownTillTheDartHeOrSheSelectsThenClicksOnIt() {
+
+        js.executeScript("arguments[0].scrollIntoView(true)", amazonLogInAccountPage.winmauDarkTungstenOk);
+        amazonLogInAccountPage.winmauDarkTungstenOk.click();
+
+
+    }
+
+
+    @Then("User sees the title of the product and the price")
+    public void userSeesTheTitleOfTheProductAndThePrice() {
+       // System.out.println("amazonLogInAccountPage.productTitle.isDisplayed() = " + amazonLogInAccountPage.productTitle.isDisplayed());
+
+    }
+
+    @Then("User clicks on the picture to zoom in")
+    public void userClicksOnThePictureToZoomIn() {
     }
 }
